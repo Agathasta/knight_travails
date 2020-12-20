effc70e7de0af26ccd2d41f66a4276bef832f3f3
agathasta <kalidonia@hotmail.com>
2020-12-20 17:56:12 +0100

    Find goal

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,24 +1,38 @@
-# class Board**
-#   init a 'board'
-#   define allowed_moves for 'move' (in board limits)
+# frozen_string_literal: true
 
-# class Knight**
-#   init start_coords
-#   define 'knight_moves'
-#   define moves from start_coords: 'possible_moves' and 'allowed_moves'
+class Path
+  def initialize(start, goal)
+    @start = start
+    @goal = goal
+    @root = Square.new(@start)
+  end
 
-# class Node
-#   init: coords & parent
+  def search_goal(root = @root, queue = [])
+    p root.possible_moves(root.coords)
+    root.possible_moves(root.coords).each do |move|
+      return puts 'Found' if move == @goal # make square and return
 
-# class Path
-#   init: found boolean, found_node, steps, start
-#   def path_to:  add Node.new(start) to children / queue []
-#                 check if child is goal, if not move to queue
-#                 from queue as parent node push children as Node.new to children
-#   def found:  path = [], until found_node.parent.nil? push found_node.parent to path,
-#               make found_node = found_node.parent
-#               print path
+      queue << Square.new(move, root)
+    end
+    return if queue.empty?
+    search_goal(queue.shift, queue)
+  end
+end
 
-# game = Board.new
-# alex = Knight.new([2,2])
-# alex.knight_moves([7,2])
+class Square
+  attr_accessor :coords, :parent
+
+  def initialize(coords, parent = nil)
+    @coords = coords
+    @parent = parent
+  end
+
+  def possible_moves(coords)
+    knight_moves = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
+    knight_moves.map { |move| [coords[0] + move[0], coords[1] + move[1]] }
+                .keep_if { |move| move.all? {|c| (0..7).include?(c) }}
+  end
+end
+
+path = Path.new([1,2], [5,4])
+path.search_goal
